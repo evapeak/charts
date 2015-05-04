7e3c4439566d2413cbf9977e2c5c28f02da99318
Lucas Cullen <lucas@bitcoinbrisbane.com>
2015-05-04 10:43:55 +1000

    .done

--- js/download.js
+++ js/download.js
@@ -38,4 +38,38 @@ $.ajax({
             series: data
         });
     }
-});
+}).done(function (data) {
+        $('#container').highcharts({
+            title: {
+                text: 'Gold USD',
+                x: -20 //center
+            },
+            subtitle: {
+                text: 'Source: BullionMark.com',
+                x: -20
+            },
+            xAxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+            yAxis: {
+                title: {
+                    text: '$'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                valueSuffix: '°C'
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: data
+        });
